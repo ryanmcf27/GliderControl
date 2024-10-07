937af1ae97536f46efb3a61cd710f343a067f761
ryanmcf7 <ryanmcf7@iastate.edu>
2024-10-07 11:58:55 -0500

    Added Arduino file and gitignore for DS_Store

---
+++ CustomControl/AutonomousDescent/AutonomousDescent.ino
@@ -0,0 +1,21 @@
+/* GLOBAL VARIABLES */
+
+
+void updateGPS() {
+  // TODO 
+}
+
+void updateIMU() {
+  // TODO
+}
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
