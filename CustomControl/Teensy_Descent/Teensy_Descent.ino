08ec6e586c21d483aa307a70ddb0b48122bc8918
ryanmcf7 <ryanmcf7@iastate.edu>
2024-11-18 10:46:37 -0600

    added dRehm's MPU6050 library and updated Teensy code IMU methods

--- CustomControl/Teensy_Descent/Teensy_Descent.ino
+++ CustomControl/Teensy_Descent/Teensy_Descent.ino
@@ -9,22 +9,21 @@
 #include <TaskScheduler.h> //for more info refer to source repository: https://github.com/arkhipenko/TaskScheduler/tree/master
 #include <TinyGPSPlus.h>
 #include <SoftwareSerial.h>
+#include "src/MPU6050/MPU6050.h"
 
 /* Method Prototypes */
 void updateGPS();
-void updateRot();
-void updateAccel();
-void updateProx();
+void updateIMU();
 void strobe();
+void calculate_IMU_error();
 
 // set up a global struct to store our sensor data
 struct SensorDataStruct {
   float gpsLat = 0.0;     //X-1's latitude (degrees)
   float gpsLng = 0.0;     //X-1's longitude (degrees)
-  float roll = 0.0;       //roll angle (degrees) - may need to add offset if CodeCell isn't mounted flat
-  float pitch = 0.0;      //pitch angle (degrees) - may need to add offset if CodeCell isn't mounted flat
-  float yaw = 0.0;        //yaw angle (degrees) - may need to add offset if CodeCell isn't mounted flat
-  //uint16_t proxDist = 0;  //distance detected by proximity sensor in <UNITS>
+  float roll = 0.0;       //roll angle (degrees) - may need to add offset if IMU isn't mounted flat
+  float pitch = 0.0;      //pitch angle (degrees) - may need to add offset if IMU isn't mounted flat
+  float yaw = 0.0;        //yaw angle (degrees) - may need to add offset if IMU isn't mounted flat
   float accelX = 0.0;     //X-1's acceleration in the X direction
   float accelY = 0.0;     //X-1's acceleration in the Y direction
   float accelZ = 0.0;     //X-1's acceleration in the Z direction
@@ -40,25 +39,30 @@ enum {PRERELEASE, DESCENT, ERROR_STATE} flightState = PRERELEASE;
 /* Constants and Pins */
 static const int RXPin = 0, TXPin = 1;
 static const uint32_t MONITOR_BAUD = 115200, GPS_BAUD = 9600;
-static const uint16_t RELEASE_PROX_THRES = 10;
+#define ACCEL_SCALE_FACTOR 1.0  //full scale accelerometer range (16384.0 for 2 G's) - reference dRehmFlight, using 1 for now to see raw IMU output
+#define GYRO_SCALE_FACTOR 1.0     //full scale accelerometer range (131.0 for 250 degrees/sec) - reference dRehmFlight, using 1 for now to see raw IMU output
+//IMU calibration parameters - calibrate IMU using calculate_IMU_error() in the void setup() to get these values, then comment out calculate_IMU_error()
+float AccErrorX = 0.0;
+float AccErrorY = 0.0;
+float AccErrorZ = 0.0;
+float GyroErrorX = 0.0;
+float GyroErrorY= 0.0;
+float GyroErrorZ = 0.0;
 
 /* Tasks */
 #define GPS_TASK_RATE 250
-#define ROT_TASK_RATE 250
-#define ACCEL_TASK_RATE 250
-//#define PROX_TASK_RATE 250
+#define IMU_TASK_RATE 250
 #define STROBE_TASK_RATE 250
 Task gpsTask(GPS_TASK_RATE, TASK_FOREVER, &updateGPS);      //update GPS global variables every GPS_TASK_RATE ms, will be enabled from the start
-Task rotTask(ROT_TASK_RATE, TASK_FOREVER, &updateRot);      //update rotation global variables every ROT_TASK_RATE ms, will be enabled from the start
-Task accelTask(ACCEL_TASK_RATE, TASK_FOREVER, &updateAccel);      //update acceleration global variables every ACCEL_TASK_RATE ms, will be enabled from the start
-//Task proxTask(PROX_TASK_RATE, TASK_FOREVER, &updateProx);   //update proximity global variables every PROX_TASK_RATE ms, will be enabled from the start
+Task imuTask(IMU_TASK_RATE, TASK_FOREVER, &updateIMU);      //update IMU global variables every IMU_TASK_RATE ms, will be enabled from the start
 Task strobeTask(STROBE_TASK_RATE, TASK_FOREVER, &strobe);   //strobe lights every STROBE_TASK_RATE ms, won't be enabled until release
 
 /* Declaring Components */
 Scheduler runner;                     //declare our task scheduler
+MPU6050 mpu6050;                      //declare MPU6050 object for our IMU
 TinyGPSPlus gps;                      //declare TinyGPSPlus object to handle GPS data (convert raw data to be more user friendly)
+SoftwareSerial ss_GPS(RXPin, TXPin);  //declare serial connection to the GPS module
 SensorDataStruct sensorData;          //declare struct to hold our sensor data
-SoftwareSerial ss_GPS(RXPin, TXPin);  //declare serial connection to the GPS module 
 
 void setup() {
   Serial.begin(MONITOR_BAUD);
@@ -69,36 +73,31 @@ void setup() {
   runner.addTask(gpsTask);
   Serial.println("added gpsTask");
   
-  runner.addTask(rotTask);
-  Serial.println("added rotTask");
-
-  runner.addTask(accelTask);
-  Serial.println("added accelTask");
-
-  //runner.addTask(proxTask);
-  //Serial.println("added proxTask");
+  runner.addTask(imuTask);
+  Serial.println("added imuTask");
 
   runner.addTask(strobeTask);
   Serial.println("added strobeTask");
 
-  delay(2500);
+  delay(1000);
   
   gpsTask.enable();
   Serial.println("Enabled gpsTask");
-  rotTask.enable();
-  Serial.println("Enabled rotTask");
-  //proxTask.enable();
-  //Serial.println("Enabled proxTask");
+  imuTask.enable();
+  Serial.println("Enabled imuTask");
 
-  delay(2000);
+  delay(1000);
+
+  calculate_IMU_error();
+  delay(1000);
 }
 
 void loop() {
   runner.execute();
   switch(flightState){
     case PRERELEASE:
-      //checks to see if X-1 has been released based on proximity sensor's data
-      //if(sensorData.proxDist >= RELEASE_PROX_THRES){
+      //checks to see if X-1 has been released based on limit switch
+      //if(---){
         //update flightState
        // flightState = DESCENT;
       //}
@@ -129,21 +128,91 @@ void updateGPS() {
   }
 }
 
-void updateRot() {
-  // Update rotation global variables
-  //myCodeCell.Motion_RotationRead(sensorData.roll, sensorData.pitch, sensorData.yaw);
-}
+void updateIMU() {
+  // Get new raw IMU values
+  int16_t AcX,AcY,AcZ,GyX,GyY,GyZ;
+  mpu6050.getMotion6(&AcX, &AcY, &AcZ, &GyX, &GyY, &GyZ);
 
-void updateAccel() {
-  // Update acceleration global variables
-  //myCodeCell.Motion_AccelerometerRead(sensorData.accelX, sensorData.accelY, sensorData.accelZ);
+  // Update IMU global variables with corrected and scaled values
+  sensorData.accelX = (AcX / ACCEL_SCALE_FACTOR) - AccErrorX;
+  sensorData.accelY = (AcY / ACCEL_SCALE_FACTOR) - AccErrorY;
+  sensorData.accelZ = (AcZ / ACCEL_SCALE_FACTOR) - AccErrorZ;
+  sensorData.roll = (GyX / GYRO_SCALE_FACTOR) - GyroErrorX;
+  sensorData.pitch = (GyY / GYRO_SCALE_FACTOR) - GyroErrorY;
+  sensorData.yaw = (GyZ / GYRO_SCALE_FACTOR) - GyroErrorZ;
 }
-
-//void updateProx() {
-  // Update proximity distance global variable
-  //sensorData.proxDist = myCodeCell.Light_ProximityRead();
-//}
 
 void strobe() {
   // TODO - method to strobe LEDs
+}
+
+
+void calculate_IMU_error() {
+  //DESCRIPTION: Computes IMU accelerometer and gyro error on startup. Note: vehicle should be powered up on flat surface
+  //Credit: dRehmFlight - https://github.com/nickrehm/dRehmFlight/blob/master/Versions/dRehmFlight_Teensy_BETA_1.3/dRehmFlight_Teensy_BETA_1.3.ino
+  /*
+   * Don't worry too much about what this is doing. The error values it computes are applied to the raw gyro and 
+   * accelerometer values AccX, AccY, AccZ, GyroX, GyroY, GyroZ in getIMUdata(). This eliminates drift in the
+   * measurement. 
+   */
+  int16_t AcX,AcY,AcZ,GyX,GyY,GyZ;  //temp vars for getting raw values from the IMU
+  float AccX, AccY, AccZ, GyroX, GyroY, GyroZ; //temp vars for scaling IMU values
+  
+  AccErrorX = 0.0;
+  AccErrorY = 0.0;
+  AccErrorZ = 0.0;
+  GyroErrorX = 0.0;
+  GyroErrorY= 0.0;
+  GyroErrorZ = 0.0;
+  
+  //Read IMU values 12000 times
+  int c = 0;
+  while (c < 12000) {
+    mpu6050.getMotion6(&AcX, &AcY, &AcZ, &GyX, &GyY, &GyZ);
+    
+    AccX  = AcX / ACCEL_SCALE_FACTOR;
+    AccY  = AcY / ACCEL_SCALE_FACTOR;
+    AccZ  = AcZ / ACCEL_SCALE_FACTOR;
+    GyroX = GyX / GYRO_SCALE_FACTOR;
+    GyroY = GyY / GYRO_SCALE_FACTOR;
+    GyroZ = GyZ / GYRO_SCALE_FACTOR;
+    
+    //Sum all readings
+    AccErrorX  = AccErrorX + AccX;
+    AccErrorY  = AccErrorY + AccY;
+    AccErrorZ  = AccErrorZ + AccZ;
+    GyroErrorX = GyroErrorX + GyroX;
+    GyroErrorY = GyroErrorY + GyroY;
+    GyroErrorZ = GyroErrorZ + GyroZ;
+    c++;
+  }
+  //Divide the sum by 12000 to get the error value
+  AccErrorX  = AccErrorX / c;
+  AccErrorY  = AccErrorY / c;
+  AccErrorZ  = AccErrorZ / c - 1.0;
+  GyroErrorX = GyroErrorX / c;
+  GyroErrorY = GyroErrorY / c;
+  GyroErrorZ = GyroErrorZ / c;
+
+  Serial.print("float AccErrorX = ");
+  Serial.print(AccErrorX);
+  Serial.println(";");
+  Serial.print("float AccErrorY = ");
+  Serial.print(AccErrorY);
+  Serial.println(";");
+  Serial.print("float AccErrorZ = ");
+  Serial.print(AccErrorZ);
+  Serial.println(";");
+  
+  Serial.print("float GyroErrorX = ");
+  Serial.print(GyroErrorX);
+  Serial.println(";");
+  Serial.print("float GyroErrorY = ");
+  Serial.print(GyroErrorY);
+  Serial.println(";");
+  Serial.print("float GyroErrorZ = ");
+  Serial.print(GyroErrorZ);
+  Serial.println(";");
+
+  Serial.println("Paste these values in user specified variables section and comment out calculate_IMU_error() in void setup.");
 }
